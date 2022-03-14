26be3639df9feb891234dbbdf674075b749014e9
Raphael Alvarenga <coreydaves@hotmail.it>
2022-03-13 21:15:48 -0300

    at the beggining  - just created all the components
    the components were created ..and then  we gonna fill them with  their stuff

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Services from './components/Services/Services';
+import Portfoglio from './components/Portfoglio/Portfoglio';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+function App() {
+
+
+
+
+
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfoglio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Portfoglio/Portfoglio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Portfoglio.css';
+
+const Portfoglio = () => {
+  return (
+    <div>Portfoglio</div>
+  )
+}
+
+export default Portfoglio
